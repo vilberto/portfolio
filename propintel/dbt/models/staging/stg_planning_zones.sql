1a7eff8b917991bd97470cc13955e0853c2133a2
Vilberto Noerjanto <vilberto@Vilbertos-MacBook-Pro.local>
2026-06-10 17:59:09 +1000

    add dbt sources and staging views for vicmap planning zones and overlays

---
+++ propintel/dbt/models/staging/stg_planning_zones.sql
@@ -0,0 +1,14 @@
+select
+    pfi,
+    lga_code,
+    lga,
+    zone_num,
+    zone_code,
+    zone_desc,
+    code_parent,
+    zncodegrp,
+    zncodegrpl,
+    gaz_b_date,
+    ufi,
+    geometry
+from {{ source('propintel', 'planning_zones') }}
