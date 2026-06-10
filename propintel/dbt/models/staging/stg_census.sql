5ddb3cb90eb6f4cde8be94456ce48b7943151f00
Vilberto Noerjanto <vilberto@Vilbertos-MacBook-Pro.local>
2026-06-10 18:27:58 +1000

    add census G02+G37 sources and stg_census staging view

---
+++ propintel/dbt/models/staging/stg_census.sql
@@ -0,0 +1,9 @@
+select
+    g02.SAL_CODE_2021                   as sal_code,
+    g02.Median_tot_hhd_inc_weekly       as median_hhd_inc_weekly,
+    g37.O_OR_Total                      as owned_outright_total,
+    g37.O_MTG_Total                     as owned_mortgage_total,
+    g37.R_Tot_Total                     as rented_total
+from {{ source('propintel', 'census_g02') }} g02
+join {{ source('propintel', 'census_g37') }} g37
+    on g02.SAL_CODE_2021 = g37.SAL_CODE_2021
